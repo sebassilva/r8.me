a8c0d3634b0f6d11f010b0536730f3454aa9b9cb
Sebastián Silva García <sebastian@Sebastians-MacBook-Air.local>
2016-12-13 14:34:27 -0600

    Updated gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 //var api = require('api.js');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '/../front/index.html'));
 });
 
 

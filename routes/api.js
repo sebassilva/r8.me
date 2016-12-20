467878480b8a1eb4a2e745ea57c6c268cdb44e64
Sebastián Silva García <sebastian@Sebastians-MacBook-Air.local>
2016-12-19 21:02:19 -0600

    Modified routing

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var userCtrl = require('../controllers/users.js');
+var postsCtrl = require('../controllers/posts.js');
 //var db = require('./db.js');
 
 
@@ -9,11 +10,24 @@ router.get('/', function(req, res){
 	res.render('index', {title: "the madafaca API"});
 });
 
-router.post('/newUser', function(req, res){
+//Users routing
+router.route('/users')
+
+.post(function(req, res){
 	userCtrl.newUser(req, res);
+})
+
+.get(function(req, res){
+	userCtrl.getUsers(req, res);
 });
 
-router.get('/users', function(req, res){
-	userCtrl.getUsers(req, res);
+
+//Posts routing
+router.route('/posts')
+.get(function(req, res){
+	postsCtrl.getPosts(req, res);
+})
+.post(function(req, res){
+	postsCtrl.newPost(req, res);
 });
 module.exports = router;
